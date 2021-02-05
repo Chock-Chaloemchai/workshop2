01e76fef7adc87c090539f9bd8cc6a30fe36d18c
Chock-Chaloemchai <Chaloemchai12372@gmail.com>
2021-02-06 00:09:20 +0700

    Add folder operators and add 11 file  in folder operators

---
+++ operators/membership4_operators.py
@@ -0,0 +1,7 @@
+name_list = ["jim", "por"]
+
+print("jim" in name_list)
+# Output : True
+
+print("chock" in name_list)
+# Output : False
---
+++ operators/membership_operators.py
@@ -0,0 +1,7 @@
+string = "Hello world"
+
+print("H" in string)
+# Output : True
+
+print("hELLO" not in string)
+# Output : True
